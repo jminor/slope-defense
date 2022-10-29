7cc8b858c7e425559b1fa1bf38b6fb25ccc8571a
Joshua Minor <jminor@users.noreply.github.com>
2022-10-29 02:18:41 -0700

    Fixed crashing. Styling.

--- src/app.cpp
+++ src/app.cpp
@@ -46,21 +46,37 @@ public:
         start_wave();
     }
 
-    value_t val_x;
-    value_t val_t;
-    symbol_table_t symbol_table;
-    expression_t expression;
-    parser_t parser;
+//    const int num_pts = 1000;
+#define num_pts 1000
+    ImPlotPoint pts[num_pts];
     bool is_valid = false;
 
+    bool show_demo = false;
+    bool show_implot_demo = false;
+
     void calculate() {
-        symbol_table.clear();
+        value_t val_x;
+        value_t val_t;
+        symbol_table_t symbol_table;
+        expression_t expression;
+        parser_t parser;
+
+//        symbol_table.clear();
         symbol_table.add_variable("x", val_x);
         symbol_table.add_variable("t", val_t);
         symbol_table.add_constants();
         expression.register_symbol_table(symbol_table);
 
         is_valid = parser.compile(gameState.formula, expression);
+
+        if (is_valid) {
+            val_t = time().as_seconds();
+            for (int i=0; i<num_pts; i++) {
+                val_x = i/(num_pts-1) * 200 - 100;
+                pts[i].x = val_x;
+                pts[i].y = expression.value();
+            }
+        }
     }
 
     void start_wave() {
@@ -87,37 +103,32 @@ public:
     void game_window() {
         ImGui::Begin("Slope Defense");
 
+        char buf[1000];
+        snprintf(buf, sizeof(buf), "%s", gameState.formula.c_str());
+        if (ImGui::InputText("Formula", buf, sizeof(buf))) {
+            gameState.formula = string(buf);
+        }
+        calculate();
+
+        ImGui::SameLine();
+
+        ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(255,0,0,255));
+        if (ImGui::Button("Fire!"))
+          print("Hello, World!");
+        ImGui::PopStyleColor();
+
+        ImGui::SameLine();
+
+        ImGui::SetNextItemWidth(100);
         if (ImGui::InputInt("Wave", &gameState.wave)) {
             gameState.wave = max(1, gameState.wave);
             start_wave();
         }
 
-        ImGui::Text("Things: %ld", gameState.things.size());
+        ImVec2 avail = ImGui::GetContentRegionAvail();
+        ImVec2 grid_size(avail.x, avail.y);
 
-        
-        char buf[1000];
-        snprintf(buf, sizeof(buf), "%s", gameState.formula.c_str());
-        if (ImGui::InputText("Formula", buf, sizeof(buf))) {
-            gameState.formula = string(buf);
-            calculate();
-        }
-
-        if (ImGui::Button("Press Me!"))
-          print("Hello, World!");
-
-        const int num_pts = 1000;
-        static ImPlotPoint pts[num_pts];
-        if (is_valid) {
-            val_t = time().as_seconds();
-            for (int i=0; i<num_pts; i++) {
-                val_x = i/(num_pts-1) * 200 - 100;
-                pts[i].x = val_x;
-                pts[i].y = expression.value();
-            }
-        }
-
-        ImVec2 grid_size(800, 800);
-
+        ImPlot::SetNextPlotLimits(-100, 100, -100, 100);
         ImPlot::BeginPlot("Game Grid", "x", "y", grid_size);
 
         ImPlot::PlotLine("sin(x)", &pts[0].x, &pts[0].y, num_pts, 0, sizeof(ImPlotPoint));
@@ -141,17 +152,49 @@ public:
         ImGui::End();
     }
 
+    void setup() {
+        ImGui::StyleColorsMahiDark1();
+        ImGuiStyle &style = ImGui::GetStyle();
+        // Main
+        style.WindowPadding    = ImVec2(8, 8);
+        style.FramePadding     = ImVec2(3, 2);
+        style.ItemSpacing      = ImVec2(4, 4);
+        style.ItemInnerSpacing = ImVec2(4, 4);
+        style.IndentSpacing    = 20.0f;
+        style.ScrollbarSize    = 15.0f;
+        style.GrabMinSize      = 5.0f;
+        // Rounding
+        style.WindowRounding    = 2.0f;
+        style.ChildRounding     = 2.0f;
+        style.FrameRounding     = 2.0f;
+        style.PopupRounding     = 2.0f;
+        style.ScrollbarRounding = 10.0f;
+        style.GrabRounding      = 2.0f;
+        style.TabRounding       = 2.0f;
+        // Alignment
+        style.WindowMenuButtonPosition = ImGuiDir_Right;
+        // Setup Dear ImGui style
+        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look
+        // identical to regular ones.
+        ImGuiIO &io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            style.WindowRounding              = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+
+        // DPI scaling method 1 (this worked for my previous project, but not this...):
+        io.FontGlobalScale *= 1.5f;
+    }
+
     void update() override {
         update_things();
 
         game_window();
 
-        static bool show_demo = true;
         if (show_demo) {
             ImGui::ShowDemoWindow(&show_demo);
         }
 
-        static bool show_implot_demo = true;
         if (show_implot_demo) {
             ImPlot::ShowDemoWindow(&show_implot_demo);
         }
@@ -164,6 +207,7 @@ int main() {
     config.decorated = false;
     config.resizable = false;
     MyApp app(config);
+    app.setup();
     app.run();
     return 0;
 }
