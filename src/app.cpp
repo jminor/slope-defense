d6e3a2483e0997c8e71fab32cf6ebdae356f28e3
Joshua Minor <jminor@users.noreply.github.com>
2022-10-29 18:13:42 -0700

    Actually playable & fun!!!!

--- src/app.cpp
+++ src/app.cpp
@@ -2,6 +2,8 @@
 #include <Mahi/Gui.hpp>
 #include <Mahi/Util.hpp>
 
+#include <imgui_internal.h>
+
 #include <exprtk.hpp>
 
 using namespace mahi::gui;
@@ -22,6 +24,7 @@ struct Thing {
     ImVec2 vel;
     float radius;
     ImColor color;
+    float pop_counter;
 };
 
 // Inherit from Application
@@ -29,9 +32,11 @@ class MyApp : public Application {
 public:
     struct GameState {
         int score;
+        float difficulty = 0;
         int wave;
         vector<Thing> things;
         string formula = "x";
+        float firing_counter;
     } gameState;
 
 
@@ -43,6 +48,7 @@ public:
         gameState.score = 0;
         gameState.wave = 1;
         gameState.things.clear();
+        gameState.firing_counter = 0;
         start_wave();
     }
 
@@ -53,6 +59,9 @@ public:
 
     bool show_demo = false;
     bool show_implot_demo = false;
+    bool show_style_editor = false;
+
+    bool set_focus = true;
 
     void calculate() {
         value_t val_x;
@@ -72,68 +81,209 @@ public:
         if (is_valid) {
             val_t = time().as_seconds();
             for (int i=0; i<num_pts; i++) {
-                val_x = i/(num_pts-1) * 200 - 100;
+                val_x = i/(value_t)(num_pts-1) * 200.0 - 100.0;
                 pts[i].x = val_x;
                 pts[i].y = expression.value();
+            }
+
+            // check for collisions
+            if (gameState.firing_counter > 0) {
+                for (auto& t : gameState.things) {
+                    // already popping?
+                    if (t.pop_counter > 0) continue;
+
+                    val_x = t.pos.x;
+                    value_t val_y = expression.value();
+
+                    if (abs(val_y - t.pos.y) < t.radius / 2) {
+                        t.pop_counter = 0.3;
+                    }
+                }
             }
         }
     }
 
     void start_wave() {
-        for (int i=0; i<gameState.wave; i++) {
+        for (int i=0; i<gameState.wave * 2 + 5; i++) {
             Thing t;
             t.pos = ImVec2(random(-100, 100), random(-100, 100));
             t.vel = ImVec2(random(-1, 1), random(-1, 1));
             t.radius = 5;
             t.color = ImColor(random(0,1), random(0,1), random(0,1), 1.0);
+            t.pop_counter = 0;
             gameState.things.push_back(t);
         }
-        calculate();
     }
 
     void update_things() {
         float dt = delta_time().as_seconds();
-        for (auto& t : gameState.things) {
-            t.pos.x += t.vel.x * dt;
-            t.pos.y += t.vel.y * dt;
+
+        auto it = gameState.things.begin();
+        while (it != gameState.things.end()) {
+
+            if (it->pop_counter > 0) {
+                it->pop_counter -= dt;
+                if (it->pop_counter <= 0) {
+                    it = gameState.things.erase(it);
+                    continue;
+                }
+
+                it->radius += 30.0 * dt;
+                it->color = ImColor(1.0f, 1.0f, 1.0f, 10*ImSaturate(it->pop_counter));
+            }
+
+            it->pos.x += it->vel.x * dt * gameState.difficulty;
+            it->pos.y += it->vel.y * dt * gameState.difficulty;
+
+            // wrap
+            if (it->pos.x < -100) it->pos.x += 200;
+            if (it->pos.y < -100) it->pos.y += 200;
+            if (it->pos.x > 100) it->pos.x -= 200;
+            if (it->pos.y > 100) it->pos.y -= 200;
+
+            ++it;
         }
 
+        // Update the graph based on the current formula
+        // Note: We do this every frame beacause it might vary based on time 't'
+        // and also because collisions are handled in there
+        calculate();
+
+        if (gameState.firing_counter > 0) {
+            gameState.firing_counter = fmaxf(0, gameState.firing_counter - dt);
+        };
+
+        if (gameState.things.size() == 0) {
+            gameState.wave++;
+            start_wave();
+        }
+    }
+
+    void main_menu() {
+        if (ImGui::BeginMainMenuBar()) {
+            if (ImGui::BeginMenu("Window")) {
+                ImGui::MenuItem("Style Editor", NULL, &show_style_editor);
+                ImGui::MenuItem("ImGui Demo", NULL, &show_demo);
+                ImGui::MenuItem("ImPlot Demo", NULL, &show_implot_demo);
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();
+        }
+    }
+
+    static int MyCallback(ImGuiInputTextCallbackData* data)
+    {
+        if (data->EventFlag == ImGuiInputTextFlags_CallbackCompletion)
+        {
+            MyApp* app = (MyApp*)data->UserData;
+            app->fire();
+        }
+    }
+
+    void fire() {
+        gameState.firing_counter = 3;
+        set_focus = true;
     }
 
     void game_window() {
-        ImGui::Begin("Slope Defense");
+        auto viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->GetWorkPos());
+        ImGui::SetNextWindowSize(viewport->GetWorkSize());
+        ImGui::Begin("Slope Defense", NULL,
+                     ImGuiWindowFlags_NoMove |
+                     ImGuiWindowFlags_NoResize |
+                     ImGuiWindowFlags_NoCollapse |
+                     ImGuiWindowFlags_NoDecoration |
+                     ImGuiWindowFlags_NoDocking
+                     );
 
-        char buf[1000];
-        snprintf(buf, sizeof(buf), "%s", gameState.formula.c_str());
-        if (ImGui::InputText("Formula", buf, sizeof(buf))) {
-            gameState.formula = string(buf);
+        main_menu();
+
+        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvailWidth()-500);
+        ImGui::BeginGroup();
+        {
+
+            ImGui::Text("Formula");
+            ImGui::Text("y = ");
+            ImGui::SameLine();
+            char buf[1000];
+            snprintf(buf, sizeof(buf), "%s", gameState.formula.c_str());
+            if (set_focus) {
+                ImGui::SetKeyboardFocusHere();
+                set_focus = false;
+            }
+            if (ImGui::InputText("##Formula", buf, sizeof(buf), ImGuiInputTextFlags_CallbackCompletion, MyCallback, this)) {
+                gameState.formula = string(buf);
+            }
+            ImGui::SetItemDefaultFocus();
+            if (ImGui::IsItemDeactivatedAfterEdit()) {
+                fire();
+            }
         }
-        calculate();
+        ImGui::EndGroup();
 
         ImGui::SameLine();
 
-        ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(255,0,0,255));
-        if (ImGui::Button("Fire!"))
-          print("Hello, World!");
-        ImGui::PopStyleColor();
+        ImGui::BeginGroup();
+        {
+            ImGui::Text(" ");
+            ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor(255,150,0,255));
+            if (ImGui::Button("Activate!")) {
+                fire();
+            }
+            ImGui::PopStyleColor();
+        }
+        ImGui::EndGroup();
+
+        ImGui::SameLine();
+        ImGui::Dummy(ImVec2(50,0));
+        ImGui::SameLine();
+
+        ImGui::BeginGroup();
+        {
+            ImGui::Text("Wave");
+            ImGui::SetNextItemWidth(ImGui::GetContentRegionAvailWidth()/2.0);
+            if (ImGui::InputInt("##Wave", &gameState.wave)) {
+                gameState.wave = max(1, gameState.wave);
+                start_wave();
+            }
+        }
+        ImGui::EndGroup();
 
         ImGui::SameLine();
 
-        ImGui::SetNextItemWidth(100);
-        if (ImGui::InputInt("Wave", &gameState.wave)) {
-            gameState.wave = max(1, gameState.wave);
-            start_wave();
+        ImGui::SetNextItemWidth(ImGui::GetContentRegionAvailWidth());
+        ImGui::BeginGroup();
+        {
+            ImGui::Text("Difficulty");
+            ImGui::SetNextItemWidth(ImGui::GetContentRegionAvailWidth());
+            if (ImGui::DragFloat("##Difficulty", &gameState.difficulty, 0.01, 0, 10)) {
+//                gameState.wave = max(1, gameState.wave);
+//                start_wave();
+            }
         }
+        ImGui::EndGroup();
 
         ImVec2 avail = ImGui::GetContentRegionAvail();
-        ImVec2 grid_size(avail.x, avail.y);
+        float size = fminf(avail.x, avail.y);
+        ImVec2 grid_size(size, size);
 
         ImPlot::SetNextPlotLimits(-100, 100, -100, 100);
-        ImPlot::BeginPlot("Game Grid", "x", "y", grid_size);
+        ImPlot::BeginPlot("Game Grid", "x", "y", grid_size,
+                          0,
+                          ImPlotAxisFlags_Lock,
+                          ImPlotAxisFlags_Lock);
 
-        ImPlot::PlotLine("sin(x)", &pts[0].x, &pts[0].y, num_pts, 0, sizeof(ImPlotPoint));
+        const ImVec4 line_normal_color = (ImVec4)ImColor(0.3f, 0.3f, 0.8f, 1.0f);
+        const ImVec4 line_firing_color = (ImVec4)ImColor(1.0f, 0.7f, 0.1f, 1.0f);
+        ImVec4 line_color = ImLerp(line_normal_color,
+                                   line_firing_color,
+                                   ImSaturate(gameState.firing_counter));
+        float line_weight = ImLerp(1.0f, 5.0f, ImSaturate(gameState.firing_counter));
+
+        ImPlot::SetNextLineStyle(line_color, line_weight);
+        ImPlot::PlotLine("", &pts[0].x, &pts[0].y, num_pts, 0, sizeof(ImPlotPoint));
         ImPlot::SetNextMarkerStyle(ImPlotMarker_Circle);
-//        ImPlot::PlotLine("x^2", xs2, ys2, 11);
 
         for (int i=0; i<gameState.things.size(); i++) {
             auto& thing = gameState.things[i];
@@ -142,8 +292,8 @@ public:
             double x = thing.pos.x;
             double y = thing.pos.y;
             if (ImPlot::DragPoint(name, &x, &y, true, thing.color, thing.radius)) {
-                thing.pos.x = x;
-                thing.pos.y = y;
+//                thing.pos.x = x;
+//                thing.pos.y = y;
             }
         }
 
@@ -153,9 +303,12 @@ public:
     }
 
     void setup() {
-        ImGui::StyleColorsMahiDark1();
+//        ImGui::StyleColorsMahiDark1();
+        ImGui::StyleColorsClassic();
+        ImPlot::StyleColorsClassic();
         ImGuiStyle &style = ImGui::GetStyle();
         // Main
+        style.WindowBorderSize = 0;
         style.WindowPadding    = ImVec2(8, 8);
         style.FramePadding     = ImVec2(3, 2);
         style.ItemSpacing      = ImVec2(4, 4);
@@ -182,8 +335,7 @@ public:
             style.Colors[ImGuiCol_WindowBg].w = 1.0f;
         }
 
-        // DPI scaling method 1 (this worked for my previous project, but not this...):
-        io.FontGlobalScale *= 1.5f;
+        io.FontGlobalScale *= 1.75f;
     }
 
     void update() override {
@@ -198,14 +350,23 @@ public:
         if (show_implot_demo) {
             ImPlot::ShowDemoWindow(&show_implot_demo);
         }
+
+        if (show_style_editor) {
+            ImGui::Begin("Style Editor", &show_style_editor);
+            ImGui::ShowStyleEditor();
+            ImGui::End();
+        }
     }
 };
 
 int main() {
     Application::Config config;
-    config.transparent = true;
-    config.decorated = false;
-    config.resizable = false;
+    config.title = "Slope Defense";
+    config.width = 1024 / 2;
+    config.height = 768 / 2;
+//    config.transparent = true;
+//    config.decorated = false;
+//    config.resizable = false;
     MyApp app(config);
     app.setup();
     app.run();
